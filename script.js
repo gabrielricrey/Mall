29037879a97977a7bdae6b41700dc6b9a34e8181
Gabriel Richter Reynoso <garey91@gmail.com>
2024-09-22 16:45:16 +0200

    alotOfChanges

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+const header = document.querySelector('header');
 const menuButton = document.getElementById('menu');
 const mobileNav = document.querySelector('.mobile-nav');
 const mobileNavLinks = document.querySelectorAll('.mobile-nav-links');
+
+document.addEventListener('scroll', () =>{
+    header.classList.toggle('scrolled', window.scrollY > header.offsetHeight)
+})
 
 menuButton.addEventListener('click', () =>{
     console.log('click');
